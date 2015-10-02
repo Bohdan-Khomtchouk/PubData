91eed0008ae0c76bf63e69109cfa1646e40395fb
Bohdan Khomtchouk <bohdankhomtchouk@UM-31HFG8WM.local>
2015-10-02 13:42:44 -0400

    BioNetHub PySide GUI

--- GUI.py
+++ GUI.py
@@ -1,8 +1,47 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Thi is a GUI version for BioNetHub command line interface, written with PySide.
-# For implementing this GUI in serveral parts we used PySide Examples (https://github.com/PySide/Examples)
+# Copyright (C) 2015 Bohdan Khomtchouk, Thor Wahlestedt, Kelly Khomtchouk, Kasra Ahmadvand, and Claes Wahlestedt
+
+# This is a GUI version for BioNetHub command line interface, written with PySide.
+
+# BioNetHub is a software program written in the Python programming language that can remotely access, search, and
+# navigate through the directory tree of any major bioinformatics database via a local computer network.
+# By assembling all major bioinformatics databases under the roof of one software program, BioNetHub allows the user to avoid
+# the unnecessary hassle and non-standardized complexities inherent to accessing databases one-by-one using
+# an Internet browser. As such, BioNetHub allows researchers to search, access, view, and download files from the FTP
+# servers of any major bioinformatics database directly from one centralized location. By using only a command-line environment
+# (e.g., Terminal), BioNetHub allows the user to simultaneously surf multiple bioinformatics FTP servers directly from the
+# comfort of their local computer. BioNetHub is designed with network programming algorithms that can patch into any
+# user-specified bioinformatics online database to be able to search, navigate, view, and download files directly from the command-line
+# from one centralized location on a local computer network.
+
+
+# BioNetHub is an ongoing bioinformatics software project fully financially supported by the
+# United States Department of Defense (DoD) through the National Defense Science and Engineering
+# Graduate Fellowship (NDSEG) Program. This research was conducted with Government support under
+# and awarded by DoD, Army Research Office (ARO), National Defense Science and Engineering
+# Graduate (NDSEG) Fellowship, 32 CFR 168a.
+
+# Please cite: "Khomtchouk et al.: 'BioNetHub: Python network programming engine for surfing the FTP servers of bioinformatics
+# databases', 2015 (in preparation)" within any source that makes use of any methods
+# inspired by BioNetHub.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# -------------------------------------------------------------------------------------------
+
 
 from PySide import QtCore, QtGui, QtNetwork
 import csv
@@ -25,7 +64,7 @@ class TabDialog(QtGui.QDialog):
         fileInfo = QtCore.QFileInfo(fileName)
 
         tabWidget = QtGui.QTabWidget()
-        tabWidget.addTab(FileInfo(fileInfo), "FileIno")
+        tabWidget.addTab(FileInfo(fileInfo), "FileInfo")
         tabWidget.addTab(ServersTab(fileInfo), "Servers")
 
         mainLayout = QtGui.QVBoxLayout()
@@ -137,7 +176,7 @@ class FtpWindow(QtGui.QDialog):
         self.cdToParentButton.setIcon(QtGui.QIcon(':/images/cdtoparent.png'))
         self.cdToParentButton.setEnabled(False)
         
-        self.serverButton = QtGui.QPushButton('server list')
+        self.serverButton = QtGui.QPushButton('Server List')
         
         self.downloadButton = QtGui.QPushButton("Download")
         self.downloadButton.setEnabled(False)
@@ -317,7 +356,7 @@ class FtpWindow(QtGui.QDialog):
 
         if self.ftp.currentCommand() == QtNetwork.QFtp.Get:
             if error:
-                self.statusLabel.setText("Canceled download of %s." % self.outFile.fileName())
+                self.statusLabel.setText("Cancelled download of %s." % self.outFile.fileName())
                 self.outFile.close()
                 self.outFile.remove()
             else:
@@ -409,7 +448,7 @@ class FtpWindow(QtGui.QDialog):
                             self.ftp.cd(path)
                             self.ftp.list()
             except IOError:
-                MESSAGE="Unfortunately this server doesn't provide a CSV file fro quick search"
+                MESSAGE="Unfortunately this server doesn't provide a CSV file for quick search"
                 reply = QtGui.QMessageBox.information(self,
                 "QMessageBox.information()", MESSAGE)
 
