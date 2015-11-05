3413e9d9b62b36e36fdc8b6770938533a7e75551
kasra <mazdakmk7@gmail.com>
2015-11-05 18:56:20 +0330

    show  that directory tree for each path after search

--- GUI.py
+++ GUI.py
@@ -176,35 +176,231 @@ class Edit_servers(QtGui.QDialog):
                 with open('Server_names.json','w') as f:
                     json.dump(self.servers,f,indent=4)
 
-class FileInfo(QtGui.QWidget):
-    def __init__(self, fileInfo, parent=None):
-        super(FileInfo, self).__init__(parent)
 
-        fileNameLabel = QtGui.QLabel("File Name:")
-        fileNameEdit = QtGui.QLineEdit(fileInfo.fileName())
+class Sub_path(QtGui.QDialog):
+    def __init__(self,ftp,root, path, parent=None):
+        super(Sub_path, self).__init__(parent)
+        self.root = root
+        self.path = path
+        self.isDirectory = {}
+        self.ftp = ftp
+        self.outFile = None
+        frameStyle = QtGui.QFrame.Sunken | QtGui.QFrame.Panel
 
-        pathLabel = QtGui.QLabel("Path:")
-        pathValueLabel = QtGui.QLabel(fileInfo.absoluteFilePath())
-        pathValueLabel.setFrameStyle(QtGui.QFrame.Panel | QtGui.QFrame.Sunken)
+        self.senameLabel = QtGui.QLabel("FTP name : ")
+        self.ftpServerLabel = QtGui.QLabel('...')
+        self.ftpServerLabel.setFrameStyle(frameStyle)
 
+        self.statusLabel = QtGui.QLabel("Please select the name of an FTP server.")
 
+        self.fileList = QtGui.QTreeWidget()
+        self.fileList.setEnabled(False)
+        self.fileList.setRootIsDecorated(False)
+        self.fileList.setHeaderLabels(("Name", "Size", "Owner", "Group", "Time"))
+        self.fileList.header().setStretchLastSection(True)
+
+        self.downloadButton = QtGui.QPushButton("Download")
+        self.cdToParentButton = QtGui.QPushButton()
+        self.cdToParentButton.setIcon(QtGui.QIcon(':/images/cdtoparent.png'))
+        self.cdToParentButton.setEnabled(False)
+        
+        self.progressDialog = QtGui.QProgressDialog(self)
+
+        self.fileList.itemActivated.connect(self.processItem)
+        self.cdToParentButton.clicked.connect(self.cdToParent)
+        self.downloadButton.clicked.connect(self.downloadFile)
+        
+        buttonBox = QtGui.QDialogButtonBox()
+        buttonBox.addButton(self.downloadButton,QtGui.QDialogButtonBox.ActionRole)
+        topLayout = QtGui.QHBoxLayout()
+        topLayout.addWidget(self.senameLabel)
+        topLayout.addWidget(self.cdToParentButton)
+        
         mainLayout = QtGui.QVBoxLayout()
-        mainLayout.addWidget(fileNameLabel)
-        mainLayout.addWidget(fileNameEdit)
-        mainLayout.addWidget(pathLabel)
-        mainLayout.addWidget(pathValueLabel)
-        mainLayout.addStretch(1)
+        mainLayout.addLayout(topLayout)
+        mainLayout.addWidget(self.fileList)
+        mainLayout.addWidget(self.statusLabel)
+        mainLayout.addWidget(buttonBox)
         self.setLayout(mainLayout)
 
+        self.setWindowTitle("BioNetHub")
+        self.setStyleSheet("""QWidget {border-radius:4px;color :black;font-weight:500; font-size: 12pt}
+        QPushButton{color:#099ff0;border-style: outset;border-width: 2px;border-radius: 10px;
+        border-color: beige;font: bold 14px;min-width: 10em;padding: 8px;}QPushButton:pressed { background-color: orange }
+        QLineEdit{background-color:white; color:black}
+        QTextEdit{background-color:#ffffff; color:#000000}QInputDialog{border-radius:4px;color :black;font-weight:500; font-size: 12pt}""")
+        self.connectOrDisconnect()
+
+
+    def sizeHint(self):
+        return QtCore.QSize(800, 400)
+
+
+    def connectOrDisconnect(self):
+        if  not self.ftp:
+            self.ftp = QtNetwork.QFtp(self)
+
+        self.setCursor(QtCore.Qt.WaitCursor)
+        self.ftp.commandFinished.connect(self.ftpCommandFinished)
+        self.ftp.listInfo.connect(self.addToList)
+        #self.ftp.dataTransferProgress.connect(self.updateDataTransferProgress)
+
+        self.fileList.clear()
+        self.currentPath = ''
+        self.isDirectory.clear()
+        print self.path
+        url = QtCore.QUrl(self.root)
+        if not url.isValid() or url.scheme().lower() != 'ftp':
+            #self.ftp.connectToHost(self.root, 21)
+            self.setCursor(QtCore.Qt.WaitCursor)
+            self.fileList.clear()
+            self.ftp.cd(self.path)
+            self.ftp.list()
+        else:
+            self.ftp.connectToHost(url.host(), url.port(21))
+
+            user_name = url.userName()
+            if user_name:
+                try:
+                    # Python v3.
+                    user_name = bytes(user_name, encoding='utf-8')
+                except:
+                    # Python v2.
+                    pass
+
+                self.ftp.login(QtCore.QUrl.fromPercentEncoding(user_name), url.password())
+            else:
+                self.ftp.login()
+
+            if url.path():
+                self.ftp.cd(url.path())
+
+        self.fileList.setEnabled(True)
+        #self.statusLabel.setText("Connecting to FTP server %s..." % self.ftpServerLabel.text())
+
+    def ftpCommandFinished(self, _, error):
+        self.setCursor(QtCore.Qt.ArrowCursor)
+
+        if self.ftp.currentCommand() == QtNetwork.QFtp.ConnectToHost:
+            if error:
+                QtGui.QMessageBox.information(self, "FTP",
+                        "Unable to connect to the FTP server at %s. Please "
+                        "check that the host name is correct." % self.ftpServerLabel.text())
+                self.connectOrDisconnect()
+                return
+
+            self.statusLabel.setText("Logged onto %s." % self.ftpServerLabel.text())
+            self.fileList.setFocus()
+            self.ftp.list()
+            self.outFile = None
+            self.progressDialog.hide()
+        elif self.ftp.currentCommand() == QtNetwork.QFtp.List:
+            if not self.isDirectory:
+                self.fileList.addTopLevelItem(QtGui.QTreeWidgetItem(["<empty>"]))
+                self.fileList.setEnabled(False)
+
+    def addToList(self, urlInfo):
+        item = QtGui.QTreeWidgetItem()
+        item.setText(0, urlInfo.name())
+        item.setText(1, str(urlInfo.size()))
+        item.setText(2, urlInfo.owner())
+        item.setText(3, urlInfo.group())
+        item.setText(4, urlInfo.lastModified().toString('MMM dd yyyy'))
+
+        if urlInfo.isDir():
+            icon = QtGui.QIcon(':/images/dir.png')
+        else:
+            icon = QtGui.QIcon(':/images/file.png')
+        item.setIcon(0, icon)
+
+        self.isDirectory[urlInfo.name()] = urlInfo.isDir()
+        self.fileList.addTopLevelItem(item)
+        if not self.fileList.currentItem():
+            self.fileList.setCurrentItem(self.fileList.topLevelItem(0))
+            self.fileList.setEnabled(True)
+
+    def processItem(self, item):
+        name = item.text(0)
+        if self.isDirectory.get(name):
+            self.fileList.clear()
+            self.isDirectory.clear()
+            self.currentPath += '/' + name
+            self.ftp.cd(name)
+            self.ftp.list()
+            self.cdToParentButton.setEnabled(True)
+            self.setCursor(QtCore.Qt.WaitCursor)
+
+    def cdToParent(self):
+        self.setCursor(QtCore.Qt.WaitCursor)
+        self.fileList.clear()
+        self.isDirectory.clear()
+
+        dirs = self.currentPath.split('/')
+        if len(dirs) > 1:
+            self.currentPath = ''
+            self.cdToParentButton.setEnabled(False)
+            self.ftp.cd('/')
+        else:
+            self.currentPath = '/'.join(dirs[:-1])
+            self.ftp.cd(self.currentPath)
+
+        self.ftp.list()
+    
+    def change_path(self,path):
+        self.ftp = QtNetwork.QFtp(self)
+        self.setCursor(QtCore.Qt.WaitCursor)
+        self.fileList.clear()
+        self.isDirectory.clear()
+        self.ftp.cd(path)
+        self.ftp.list()
+
+    def updateDataTransferProgress(self, readBytes, totalBytes):
+        self.progressDialog.setMaximum(totalBytes)
+        self.progressDialog.setValue(readBytes)
+
+    def enableDownloadButton(self):
+        current = self.fileList.currentItem()
+        if current:
+            currentFile = current.text(0)
+            self.downloadButton.setEnabled(not self.isDirectory.get(currentFile))
+        else:
+            self.downloadButton.setEnabled(False)
+    def downloadFile(self):
+        fileName = self.fileList.currentItem().text(0)
+
+        if QtCore.QFile.exists(fileName):
+            QtGui.QMessageBox.information(self, "FTP",
+                    "There already exists a file called %s in the current "
+                    "directory." % fileName)
+            return
+
+        self.outFile = QtCore.QFile(fileName)
+        if not self.outFile.open(QtCore.QIODevice.WriteOnly):
+            QtGui.QMessageBox.information(self, "FTP",
+                    "Unable to save the file %s: %s." % (fileName, self.outFile.errorString()))
+            self.outFile = None
+            return
+
+        print self.fileList.currentItem().text(0)
+        self.ftp.get(self.fileList.currentItem().text(0), self.outFile)
+
+        self.progressDialog.setLabelText("Downloading %s..." % fileName)
+        self.downloadButton.setEnabled(False)
+        self.progressDialog.exec_()
+
+    def cancelDownload(self):
+        self.ftp.abort()
+
 class Path_results(QtGui.QDialog):    
-    def __init__(self,pathes,parent=None):
+    def __init__(self,ftp,pathes,basename,parent=None):
         super(Path_results, self).__init__(parent)                
         self.mainLayout = QtGui.QVBoxLayout()
         self.setLayout(self.mainLayout)
         self.pathes=pathes
+        self.basename=basename
         self.hLayout = QtGui.QHBoxLayout()
         self.mainLayout.insertLayout(0, self.hLayout)
-
+        self.ftp = ftp
         self.listA=QtGui.QTreeWidget()
         self.listA.setHeaderLabels(['Matched Paths'])
         self.dialogbox=QtGui.QInputDialog()
@@ -215,7 +411,6 @@ class Path_results(QtGui.QDialog):
             self.listA.addTopLevelItem(item)
 
         self.hLayout.addWidget(self.listA)
-
         self.setStyleSheet("""QWidget {border-radius:4px;color :black;font-weight:500; font-size: 12pt}
         QPushButton{color:#099ff0;border-style: outset;border-width: 2px;border-radius: 10px;
         border-color: beige;font: bold 14px;min-width: 10em;padding: 8px;}QPushButton:pressed { background-color: orange }
@@ -226,9 +421,10 @@ class Path_results(QtGui.QDialog):
 
     @pyqtSlot(QtGui.QTreeWidget)
     def doubleClicked_path(self,item):
-        self.close
-        FT=FtpWindow()
-        FT.change_path(item.text(0))
+        self.wind = Sub_path(self.ftp,self.basename,item.text(0))
+        self.wind.resize(450, 650)
+        self.wind.setWindowTitle('Sub Path')
+        self.wind.show()
 
 
 def pickle_self(arg):
@@ -338,7 +534,7 @@ class FtpWindow(QtGui.QDialog):
         
         except IOError:
             print("Can't find server file.")
-            with open('Server_names.json','w')as f:
+            with open('Server_names.json','w') as f:
                 json.dump(server_names,f,indent=4)
                 MESSAGE="""<p>Couldn't find the servers file.</p>
                 <p>Server names has beed rewrite, you can try again.</p>"""
@@ -426,7 +622,7 @@ class FtpWindow(QtGui.QDialog):
             self.outFile = None
             return
 
-        print(self.fileList.currentItem().text(0))
+        print self.fileList.currentItem().text(0)
         self.ftp.get(self.fileList.currentItem().text(0), self.outFile)
 
         self.progressDialog.setLabelText("Downloading %s..." % fileName)
@@ -576,18 +772,19 @@ class FtpWindow(QtGui.QDialog):
                 self.isDirectory.clear()
                 self.setCursor(QtCore.Qt.WaitCursor)
                 self.word=text
-                self.ftpu = ftputil.FTPHost('mirbase.org','anonymous','')
+                self.ftpu = ftputil.FTPHost(self.ftpServerLabel.text(),'anonymous','')
                 self.leading = ['/'+i for i in self.ftpu.listdir(self.ftpu.curdir) if not self.ftpu.path.isfile(i)]
                 self.length=len(self.leading)
                 self.regural_search()
 
     def Traverse(self,root):
 
-        ftp = ftputil.FTPHost('mirbase.org','anonymous','')
+        ftp = ftputil.FTPHost(self.ftpServerLabel.text(),'anonymous','')
         recursive = ftp.walk(root,topdown=True,onerror=None)
         for path,_,files in recursive:
             if any(self.word in i.lower() for i in files):
                 self.all_path.append(path)
+                break
     
 
     def regural_search(self):
@@ -595,12 +792,10 @@ class FtpWindow(QtGui.QDialog):
         results = workers.map(pickle_self,self.leading)
 
         if results:
-            self.wid = Path_results(self.all_path)
+            self.wid = Path_results(self.ftp,self.all_path,self.ftpServerLabel.text())
             self.wid.resize(350, 650)
             self.wid.setWindowTitle('NewWindow')
             self.wid.show()
-
-
 
 
 
@@ -612,9 +807,3 @@ if __name__ == '__main__':
     ftpWin = FtpWindow()
     ftpWin.show()
     sys.exit(ftpWin.exec_())
-
-
-
-
-
-
