04eb298de2df7ba55cf65280090f1b1c09cc2722
Bohdan Khomtchouk <bohdankhomtchouk@UM-31HFG8WM.local>
2016-02-28 20:53:31 -0600

    updated headers

--- csv_creator.py
+++ csv_creator.py
@@ -1,40 +1,16 @@
 #!/usr/bin/env python
-# Copyright (C) 2015 Bohdan Khomtchouk, Thor Wahlestedt, Kelly Khomtchouk, Kasra Ahmadvand, and Claes Wahlestedt
 
-# BioNetHub is a software program written in the Python programming language that can remotely access and
-# navigate through the directory tree of any major bioinformatics database via a local computer network.
-# By assembling all major bioinformatics databases under the roof of one software program, BioNetHub allows the user to avoid
-# the unnecessary hassle and non-standardized complexities inherent to accessing databases one-by-one using
-# an Internet browser. As such, BioNetHub allows researchers to access, view, and download files from the FTP
-# servers of any major bioinformatics database directly from one centralized location. By using only a command-line environment
-# (e.g., Terminal), BioNetHub allows the user to simultaneously surf multiple bioinformatics FTP servers directly from the
-# comfort of their local computer. BioNetHub is designed with network programming algorithms that can patch into any
-# user-specified bioinformatics online database to be able to navigate, view, and download files directly from the command-line
-# from one centralized location on a local computer network.
+# Copyright (C) 2015-2016 Bohdan Khomtchouk, Kasra Ahmadvand, Thor Wahlestedt, Kelly Khomtchouk, Vytas Dargis-Robinson, and Claes Wahlestedt
 
-
-# BioNetHub is an ongoing bioinformatics software project fully financially supported by the
-# United States Department of Defense (DoD) through the National Defense Science and Engineering
-# Graduate Fellowship (NDSEG) Program. This research was conducted with Government support under
-# and awarded by DoD, Army Research Office (ARO), National Defense Science and Engineering
-# Graduate (NDSEG) Fellowship, 32 CFR 168a.
-
-# Please cite: "Khomtchouk et al.: 'BioNetHub: Python network programming engine for surfing the FTP servers of bioinformatics
-# databases', 2015 (in preparation)" within any source that makes use of any methods
-# inspired by BioNetHub.
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# PubData is a genomics search engine for all bioinformatics databases worldwide.  PubData searches biomedical FTP data in a user-friendly fashion similar to how PubMed searches biomedical literature.  PubData is hosted as a standalone GUI software program, while PubMed is hosted as an online web server.  PubData is built on novel network programming and natural language processing algorithms that can patch into any user-specified bioinformatics online database and query its contents.
+# Future plans include adding web server support for PubData, and contributions from the open source community are welcome.
+# PubData is designed as a graphical user interface (GUI) software program written in the Python programming language and PySide (Python binding of the cross-platform GUI toolkit Qt).  PubData can remotely search, access, view, and download files from the deeply nested directory trees of any major bioinformatics database via a local computer network.  
+# By assembling all major bioinformatics databases under the roof of one software program, PubData allows the user to avoid the unnecessary hassle and non-standardized complexities inherent to accessing databases one-by-one using an Internet browser.  More importantly, it allows a user to query multiple databases simultaneously for user-specified keywords (e.g., `human`, `cancer`, `transcriptome`).  As such, PubData allows researchers to search, access, view, and download files from the FTP servers of any major bioinformatics database directly from one centralized location.  By using only a GUI, PubData allows the user to simultaneously surf multiple bioinformatics FTP servers directly from the comfort of their local computer.
+# PubData is an ongoing bioinformatics software project financially supported by the United States Department of Defense (DoD) through the National Defense Science and Engineering Graduate Fellowship (NDSEG) Program. This research was conducted with Government support under and awarded by DoD, Army Research Office (ARO), National Defense Science and Engineering Graduate (NDSEG) Fellowship, 32 CFR 168a.
+# Please cite: "Khomtchouk et al.: 'PubData: genomics search engine for bioinformatics databases', 2016 (in preparation)" within any source that makes use of any methods inspired by PubData.
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # -------------------------------------------------------------------------------------------
 
