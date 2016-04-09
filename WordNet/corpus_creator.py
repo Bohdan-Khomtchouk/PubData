0ce55a9096e5e75c5ab78a717c1dfb0fd07ca661
kasra <mazdakmk7@gmail.com>
2016-04-09 22:28:40 +0430

    Wrap the functions in a class and filter the words

--- WordNet/corpus_creator.py
+++ WordNet/corpus_creator.py
@@ -3,15 +3,24 @@ from nltk import word_tokenize, pos_tag
 import codecs
 
 
-def create_dict():
-    with codecs.open('pdf_parsing/final_result.json', encoding='UTF-8') as f:
-        words = json.load(f)
-    new_dict = {k: [word for word, tag in pos_tag(word_tokenize(v)) if tag == 'NN'] for k, v in words.items()}
-    return new_dict
+class Corpus():
+    def __init__(self, *args, **kwargs):
+        self.input_file = kwargs['input_file']
+        self.output_file = kwargs['output_file']
 
-def create_json():
-    tagged_dict = create_dict()
-    with codecs.open('corpus.json', 'w', encoding='UTF-8') as f:
-        json.dump(tagged_dict, f, indent=4)
+    def create_dict(self):
+        with codecs.open(self.input_file, encoding='UTF-8') as f:
+            words = json.load(f)
+        new_dict = {k: [word for word, tag in pos_tag(word_tokenize(v)) if tag == 'NN' and len(word)>3] for k, v in words.items()}
+        return new_dict
 
-create_json()
+    def create_json(self):
+        tagged_dict = self.create_dict()
+        with codecs.open(self.output_file, 'w', encoding='UTF-8') as f:
+            json.dump(tagged_dict, f, indent=4)
+
+
+if __name__ == "__main__":
+    Crp = Corpus(input_file='pdf_parsing/final_result.json',
+                 output_file='corpus.json')
+    Crp.create_json()
