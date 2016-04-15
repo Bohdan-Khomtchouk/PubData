018c99d2a6e3a39362fe8e5eec7a1d3333040a76
kasra <mazdakmk7@gmail.com>
2016-04-15 16:37:06 +0430

    change path of images and josn files cum trimming the long lines

--- src/GUI.py
+++ src/GUI.py
@@ -1,21 +1,39 @@
-#!/usr/bin/env python
+"""
+Copyright (C) 2015-2016 Bohdan Khomtchouk, Kasra Ahmadvand, Thor Wahlestedt, Kelly Khomtchouk,
+Vytas Dargis-Robinson, Claes Wahlestedt.
 
-# Copyright (C) 2015-2016 Bohdan Khomtchouk, Kasra Ahmadvand, Thor Wahlestedt, Kelly Khomtchouk, Vytas Dargis-Robinson, Claes Wahlestedt
+PubData is a genomics search engine and file retrieval system for all bioinformatics databases worldwide.
+PubData searches biomedical FTP data in a user-friendly fashion similar to how PubMed searches biomedical literature.
+PubData is hosted as a standalone GUI software program, while PubMed is hosted as an online web server.
+PubData is built on novel network programming and natural language processing algorithms that can
+patch into the FTP servers of any user-specified bioinformatics database, query its contents, and retrieve files for download.
+Future plans include adding web server support for PubData, and contributions from the open source community are welcome.
+PubData is designed as a graphical user interface (GUI) software program written in the Python programming language
+and PySide (Python binding of the cross-platform GUI toolkit Qt).
+PubData can remotely search, access, view, and retrieve files from the deeply nested directory trees
+of any major bioinformatics database via a local computer network.
+By assembling all major bioinformatics databases under the roof of one software program, PubData allows the user
+to avoid the unnecessary hassle and non-standardized complexities inherent to accessing databases one-by-one using an Internet browser.
+More importantly, it allows a user to query multiple databases simultaneously for user-specified keywords
+(e.g., `human`, `cancer`, `transcriptome`).  As such, PubData allows researchers to search, access, view, and retrieve files
+from the FTP servers of any major bioinformatics database directly from one centralized location.
+By using only a GUI, PubData allows the user to simultaneously surf multiple bioinformatics FTP servers directly from the comfort of their local computer.
+PubData is an ongoing bioinformatics software project financially supported by the United States Department of Defense (DoD)
+through the National Defense Science and Engineering Graduate Fellowship (NDSEG) Program.
+This research was conducted with Government support under and awarded by DoD, Army Research Office (ARO),
+National Defense Science and Engineering Graduate (NDSEG) Fellowship, 32 CFR 168a.
+Please cite: "Khomtchouk et al.: 'PubData: genomics search engine for bioinformatics databases', 2016 (in preparation)" within any source
+that makes use of any methods inspired by PubData.
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with this program.
+If not, see <http://www.gnu.org/licenses/>.
+"""
 
-# PubData is a genomics search engine and file retrieval system for all bioinformatics databases worldwide.  PubData searches biomedical FTP data in a user-friendly fashion similar to how PubMed searches biomedical literature.  PubData is hosted as a standalone GUI software program, while PubMed is hosted as an online web server.  PubData is built on novel network programming and natural language processing algorithms that can patch into the FTP servers of any user-specified bioinformatics database, query its contents, and retrieve files for download.
-# Future plans include adding web server support for PubData, and contributions from the open source community are welcome.
-# PubData is designed as a graphical user interface (GUI) software program written in the Python programming language and PySide (Python binding of the cross-platform GUI toolkit Qt).  PubData can remotely search, access, view, and retrieve files from the deeply nested directory trees of any major bioinformatics database via a local computer network.  
-# By assembling all major bioinformatics databases under the roof of one software program, PubData allows the user to avoid the unnecessary hassle and non-standardized complexities inherent to accessing databases one-by-one using an Internet browser.  More importantly, it allows a user to query multiple databases simultaneously for user-specified keywords (e.g., `human`, `cancer`, `transcriptome`).  As such, PubData allows researchers to search, access, view, and retrieve files from the FTP servers of any major bioinformatics database directly from one centralized location.  By using only a GUI, PubData allows the user to simultaneously surf multiple bioinformatics FTP servers directly from the comfort of their local computer.
-# PubData is an ongoing bioinformatics software project financially supported by the United States Department of Defense (DoD) through the National Defense Science and Engineering Graduate Fellowship (NDSEG) Program. This research was conducted with Government support under and awarded by DoD, Army Research Office (ARO), National Defense Science and Engineering Graduate (NDSEG) Fellowship, 32 CFR 168a.
-# Please cite: "Khomtchouk et al.: 'PubData: genomics search engine for bioinformatics databases', 2016 (in preparation)" within any source that makes use of any methods inspired by PubData.
-# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-# -------------------------------------------------------------------------------------------
-
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 
 import ftplib
 import json
@@ -25,7 +43,7 @@ import socket
 from Queue import Queue
 from itertools import chain
 from threading import Thread
-
+from stylesheet import general_style
 from PyQt4.QtCore import pyqtSlot
 
 from PySide import QtCore, QtGui, QtNetwork
@@ -108,11 +126,7 @@ class Edit_servers(QtGui.QDialog):
         self.button_layout.addWidget(ok_button)
 
         self.main_layout.addWidget(self.button_group_box)
-        self.setStyleSheet("""QWidget {border-radius:4px;color :black;font-weight:500; font-size: 12pt}
-        QPushButton{color:#099ff0;border-style: outset;border-width: 2px;border-radius: 10px;
-        border-color: beige;font: bold 14px;min-width: 10em;padding: 8px;}QPushButton:pressed { background-color: orange }
-        QLineEdit{background-color:white; color:black}QTextEdit{background-color:#ffffff; color:#000000}
-        QInputDialog {border-radius:4px;color :black;font-weight:500; font-size: 12pt}QTreeWidgetItem{background-color:white; color:black}""")
+        self.setStyleSheet(general_style)
 
         self.list_a.itemDoubleClicked.connect(self.doubleClickedSlot)
 
@@ -249,7 +263,7 @@ class Sub_path(QtGui.QDialog):
 
         self.downloadButton = QtGui.QPushButton("Download")
         self.cdToParentButton = QtGui.QPushButton()
-        self.cdToParentButton.setIcon(QtGui.QIcon('images/cdtoparent.png'))
+        self.cdToParentButton.setIcon(QtGui.QIcon('../images/cdtoparent.png'))
         self.cdToParentButton.setEnabled(False)
 
         self.progressDialog = QtGui.QProgressDialog(self)
@@ -272,11 +286,7 @@ class Sub_path(QtGui.QDialog):
         self.setLayout(main_layout)
 
         self.setWindowTitle("PubData")
-        self.setStyleSheet("""QWidget {border-radius:4px;color :black;font-weight:500; font-size: 12pt}
-        QPushButton{color:#099ff0;border-style: outset;border-width: 2px;border-radius: 10px;
-        border-color: beige;font: bold 14px;min-width: 10em;padding: 8px;}QPushButton:pressed { background-color: orange }
-        QLineEdit{background-color:white; color:black}
-        QTextEdit{background-color:#ffffff; color:#000000}QInputDialog{border-radius:4px;color :black;font-weight:500; font-size: 12pt}""")
+        self.setStyleSheet(general_style)
         self.connectOrDisconnect()
 
     def connectOrDisconnect(self):
@@ -381,9 +391,9 @@ class Sub_path(QtGui.QDialog):
         item.setText(4, urlInfo.lastModified().toString('MMM dd yyyy'))
 
         if urlInfo.isDir():
-            icon = QtGui.QIcon('images/dir.png')
+            icon = QtGui.QIcon('../images/dir.png')
         else:
-            icon = QtGui.QIcon('images/file.png')
+            icon = QtGui.QIcon('../images/file.png')
         item.setIcon(0, icon)
 
         self.isDirectory[urlInfo.name()] = urlInfo.isDir()
@@ -570,11 +580,7 @@ class Path_results(QtGui.QDialog):
                 self.list_a.addTopLevelItem(item)
 
         self.h_layout.addWidget(self.list_a)
-        self.setStyleSheet("""QWidget {border-radius:4px;color :black;font-weight:500; font-size: 12pt}
-        QPushButton{color:#099ff0;border-style: outset;border-width: 2px;border-radius: 10px;
-        border-color: beige;font: bold 14px;min-width: 10em;padding: 8px;}QPushButton:pressed { background-color: orange }
-        QLineEdit{background-color:white; color:black}QTextEdit{background-color:#ffffff; color:#000000}
-        QInputDialog {border-radius:4px;color :black;font-weight:500; font-size: 12pt}QTreeWidgetItem{background-color:white; color:black}""")
+        self.setStyleSheet(general_style)
 
         self.list_a.itemDoubleClicked.connect(self.doubleClicked_path)
 
@@ -651,11 +657,7 @@ class SelectServers(QtGui.QDialog):
         self.button_layout.addWidget(self.ok_button)
 
         self.main_layout.addWidget(self.button_group_box)
-        self.setStyleSheet("""QWidget {border-radius:4px;color :black;font-weight:500; font-size: 12pt}
-        QPushButton{color:#099ff0;border-style: outset;border-width: 2px;border-radius: 10px;
-        border-color: beige;font: bold 14px;min-width: 10em;padding: 8px;}QPushButton:pressed { background-color: orange }
-        QLineEdit{background-color:white; color:black}QTextEdit{background-color:#ffffff; color:#000000}
-        QInputDialog {border-radius:4px;color :black;font-weight:500; font-size: 12pt}QTreeWidgetItem{background-color:white; color:black}""")
+        self.setStyleSheet(general_style)
 
     def get_selected_servers(self):
         """
@@ -865,7 +867,7 @@ class ftpWindow(QtGui.QDialog):
         self.connectButton.setDefault(True)
 
         self.cdToParentButton = QtGui.QPushButton()
-        self.cdToParentButton.setIcon(QtGui.QIcon('images/cdtoparent.png'))
+        self.cdToParentButton.setIcon(QtGui.QIcon('../images/cdtoparent.png'))
         self.cdToParentButton.setEnabled(False)
 
         self.serverButton = QtGui.QPushButton('server list')
@@ -921,13 +923,7 @@ class ftpWindow(QtGui.QDialog):
         main_layout.addWidget(button_box)
         self.setLayout(main_layout)
         self.setWindowTitle("PubData")
-        self.setStyleSheet("""QWidget {border-radius:4px;color :black;font-weight:500; font-size: 12pt}
-        QPushButton{color:#099ff0;border-style: outset;border-width: 2px;border-radius: 10px;
-        border-color: beige;font: bold 14px;min-width: 10em;padding: 8px;}
-        QPushButton:pressed { background-color: orange }
-        QLineEdit{background-color:white; color:black}
-        QTextEdit{background-color:#ffffff; color:#000000}
-        QInputDialog{border-radius:4px; color:black; font-weight:500; font-size:12pt}""")
+        self.setStyleSheet(general_style)
 
     def getServerNames(self):
         """
@@ -1232,9 +1228,9 @@ class ftpWindow(QtGui.QDialog):
         item.setText(4, urlInfo.lastModified().toString('MMM dd yyyy'))
 
         if urlInfo.isDir():
-            icon = QtGui.QIcon('images/dir.png')
+            icon = QtGui.QIcon('../images/dir.png')
         else:
-            icon = QtGui.QIcon('images/file.png')
+            icon = QtGui.QIcon('../images/file.png')
         item.setIcon(0, icon)
 
         self.isDirectory[urlInfo.name()] = urlInfo.isDir()
