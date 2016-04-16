6cdec9ba9d0fb6f5cfbe1b43cb03faf385c67a55
kasra <mazdakmk7@gmail.com>
2016-04-16 22:19:23 +0430

    Change the architecture

--- src/stylesheet.py
+++
@@ -1,8 +0,0 @@
-
-general_style = """QWidget {border-radius:4px;color :black;font-weight:500; font-size: 12pt}
-        QPushButton{color:#099ff0;border-style: outset;border-width: 2px;border-radius: 10px;
-        border-color: beige;font: bold 14px;min-width: 10em;padding: 8px;}
-        QPushButton:pressed { background-color: orange }
-        QLineEdit{background-color:white; color:black}
-        QTextEdit{background-color:#ffffff; color:#000000}
-        QInputDialog{border-radius:4px; color:black; font-weight:500; font-size:12pt}"""
