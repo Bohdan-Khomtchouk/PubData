a35e609d871e33f6becd245782d839d03fb50399
kasra <mazdakmk7@gmail.com>
2016-04-27 07:44:23 +0430

    Convert ftpwalker to a module

--- interface/ftpwalker.py
+++ interface/ftpwalker.py
@@ -7,27 +7,26 @@
 # -------------------------------------------------------------------------------------------
 
 import ftplib
-from os import path, mkdir
+from os import path
 from threading import Thread
 from Queue import Queue
 from datetime import datetime
 import json
 import socket
-import re
 
 
 class ftp_walker(object):
-    def __init__(self, server_addr):
+    def __init__(self, root, server_addr):
         self.length = 2
         self.server_addr = server_addr
         self.all_path = Queue()
-        self.base, self.leading = self.find_leading()
+        self.base, self.leading = self.find_leading(root)
 
-    def find_leading(self):
+    def find_leading(self, root):
         base = []
         conn = ftplib.FTP(self.server_addr)
         conn.login()
-        for p, dirs, files in self.Walk(conn, '/'):
+        for p, dirs, files in self.Walk(conn, root):
             length = len(dirs)
             base.append((p, files))
             if length > 1:
@@ -60,14 +59,13 @@ class ftp_walker(object):
             for x in self.Walk(connection, new_path):
                 yield x
 
-    def Traverse(self, first_path, _path='/'):
+    def Traverse(self, _path='/'):
         try:
             connection = ftplib.FTP(self.server_addr)
             connection.login()
         except Exception as exp:
             print 'Connection failed for path : ', _path, exp.__str__()
         else:
-            first_path = re.sub(r'([./])', '\\\1', first_path)
             for _path, _, files in self.Walk(connection, _path):
                 self.all_path.put((_path, files))
                 print _path
@@ -75,40 +73,39 @@ class ftp_walker(object):
 
     def run(self, threads=[]):
         print 'start threads...'
-        first_part = self.leading[:4]
-        second_part = self.leading[4:]
-        parts = (first_part, second_part) if len(first_part) > 4 else (first_part,)
+        parts = [self.leading[i:i + 4] for i in range(0, len(self.leading), 4)]
         for part in parts:
             for conn in part:
-                thread = Thread(target=self.Traverse, args=(conn.strip('/'), conn,))
+                thread = Thread(target=self.Traverse, args=(conn,))
                 thread.start()
                 threads.append(thread)
             for thread in threads:
                     thread.join()
 
-if __name__ == "__main__":
 
-    server_names = {'Ensembl Genome Browser': 'ftp.ensembl.org',
-                    'The Arabidopsis Information Resource': 'ftp.arabidopsis.org/home',
-                    'National Center for Biotechnology Information': 'ftp.ncbi.nlm.nih.gov',
-                    'O-GLYCBASE': 'ftp.cbs.dtu.dk',
-                    'PairsDB': 'nic.funet.fi'}
 
-    for i, j in server_names.items():
-        print '---' * 5, datetime.now(), '{}'.format(j), '---' * 5
-        try:
-            FT = ftp_walker(j)
-            FT.run()
-        except (ftplib.error_temp, ftplib.error_perm, socket.gaierror) as exp:
-            print exp
-        else:
-            print '***' * 5, datetime.now(), '***' * 5
-            l = []
-            print 'creating list...'
-            while FT.all_path.qsize() > 0:
-                l.append(FT.all_path.get())
-            print 'creating dict...'
-            d = dict(FT.base + l)
-            print 'writing to json...'
-            with open('{}.json'.format(i), 'w') as fp:
-                json.dump(d, fp, indent=4, encoding='latin1')
+"""server_names = {'Ensembl Genome Browser': 'ftp.ensembl.org',
+                'The Arabidopsis Information Resource': 'ftp.arabidopsis.org/home',
+                'National Center for Biotechnology Information': 'ftp.ncbi.nlm.nih.gov',
+                'O-GLYCBASE': 'ftp.cbs.dtu.dk',
+                'PairsDB': 'nic.funet.fi'}"""
+
+
+def main_run(address, name, root):
+    print '---' * 5, datetime.now(), '{}'.format(address), '---' * 5
+    try:
+        FT = ftp_walker(root, address)
+        FT.run()
+    except (ftplib.error_temp, ftplib.error_perm, socket.gaierror) as exp:
+        print exp
+    else:
+        print '***' * 5, datetime.now(), '***' * 5
+        l = []
+        print 'creating list...'
+        while FT.all_path.qsize() > 0:
+            l.append(FT.all_path.get())
+        print 'creating dict...'
+        d = dict(FT.base + l)
+        print 'writing to json...'
+        with open('{}.json'.format(name), 'w') as fp:
+            json.dump(d, fp, indent=4, encoding='latin1')
