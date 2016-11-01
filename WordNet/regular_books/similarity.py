abb276df44b90ac7f74f50f561f8d1d55cdeef52
kasramvd <kasraavand@gmail.com>
2016-11-02 00:06:11 +0330

    Improve the perfromancec, use numy instead of python, and other improvements

--- WordNet/regular_books/similarity.py
+++ WordNet/regular_books/similarity.py
@@ -8,26 +8,19 @@
 import numpy as np
 from itertools import chain
 from functools import wraps
-from collections import Counter
+from collections import Counter, defaultdict
 from itertools import permutations
 from os import path as ospath
+from operator import itemgetter
 import json
 import glob
 
 
 class Initializer:
     def __init__(self, *args, **kwargs):
-        self.main_dict = kwargs["main_dict"]
+        self.main_dict = {i: set(j) for i, j in kwargs["main_dict"].items() if j}
         self.all_words = self.create_words()
-        self.w_w_i = self.create_words_with_indices()
         self.all_sent = self.get_sentences()
-        self.s_w_i = self.create_sentences_with_indices()
-
-    def create_words_with_indices(self):
-        return {w: i for i, w in enumerate(self.all_words)}
-
-    def create_sentences_with_indices(self):
-        return {s: i for i, s in enumerate(self.all_sent)}
 
     def create_words(self):
         all_words = np.unique(np.fromiter(chain.from_iterable(self.main_dict.values()),
@@ -35,25 +28,14 @@ class Initializer:
         return all_words
 
     def get_sentences(self):
-        return list(self.main_dict)
-
-    def create_WSM(self):
-        """
-        Initialize the word similarity matrix by creating a matrix with
-        1 as its main digonal and columns with word names.
-        """
-        dt = np.dtype({"names": self.all_words, "formats": [np.float16] * self.all_words.size})
-        wsm = np.zeros(self.all_words.size, dtype=dt)
-        wsm_view = wsm.view(np.float16).reshape(self.all_words.size, -1)
-        np.fill_diagonal(wsm_view, 1)
-        return wsm
+        return np.array(list(self.main_dict))
 
     def create_SSM(self):
         """
         Initialize the sentence similarity matrix by creating a NxN zero filled
         matrix where N is the number of sentences.
         """
-        size = len(self.all_sent)
+        size = self.all_sent.size
         dt = np.dtype({"names": self.all_sent,
                        "formats": [np.float16] * size})
         return np.zeros(size, dtype=dt)
@@ -125,15 +107,55 @@ class FindSimilarity(Initializer):
             self.iteration_number = args[0]
         except IndexError:
             raise Exception("Please provide an iteration number!")
+        self.name = ospath.basename(kwargs["name"]).split('.')[0]
+        # Word and the indices of its sentences.
+        self.sentence_with_indices = self.create_sentence_with_indices()
+        # Sentences and the indices of their words
+        self.word_with_indices = self.create_words_with_indices()
         self.latest_WSM = self.create_WSM()
         self.latest_SSM = self.create_SSM()
-        self.name = ospath.basename(kwargs["name"]).split('.')[0]
+        self.counter = Counter(self.all_words)
+        self.sum5 = sum(j for _, j in self.counter.most_common(5))
+        print("Finish initialization...!")
+
+    def create_words_with_indices(self):
+        w_w_i = {w: i for i, w in enumerate(self.all_words)}
+        total = {}
+        for s, words in self.main_dict.items():
+            if len(words) > 1:
+                total[s] = list(itemgetter(*words)(w_w_i))
+            elif words:
+                total[s] = w_w_i[next(iter(words))]
+        return total
+
+    def create_sentence_with_indices(self):
+        s_w_i = {s: i for i, s in enumerate(self.all_sent)}
+        total = {}
+        for w in self.all_words:
+            sentences = self.sentence_include_word(w)
+            if len(sentences) > 1:
+                total[w] = list(itemgetter(*sentences)(s_w_i))
+            elif sentences:
+                total[w] = [s_w_i[sentences.pop()]]
+        return total
+
+    def create_WSM(self):
+        """
+        Initialize the word similarity matrix by creating a matrix with
+        1 as its main digonal and columns with word names.
+        """
+        dt = np.dtype({"names": self.all_words, "formats": [np.float16] * self.all_words.size})
+        wsm = np.zeros(self.all_words.size, dtype=dt)
+        wsm_view = wsm.view(np.float16).reshape(self.all_words.size, -1)
+        np.fill_diagonal(wsm_view, 1)
+        return wsm
 
     def affinity_WS(self, W, S, n):
-        return max(self.WSM(n)[W][self.w_w_i[wi]] for wi in self.main_dict[S])
+        # return max(self.WSM(n)[W][self.w_w_i[w]] for w in self.main_dict[S])
+        return self.WSM(n)[W][self.word_with_indices[S]].max()
 
     def affinity_SW(self, S, W, n):
-        return max(self.SSM(n)[S][self.s_w_i[sj]] for sj in self.sentence_include_word(W))
+        return self.SSM(n)[S][self.sentence_with_indices[W]].max()
 
     def similarity_W(self, W1, W2, n):
         return sum(self.weight(s=s, w=W1) * self.affinity_SW(s, W2, n - 1)
@@ -143,19 +165,23 @@ class FindSimilarity(Initializer):
         return sum(self.weight(w=w, s=S1) * self.affinity_WS(w, S2, n - 1) for w in self.main_dict[S1])
 
     def sentence_include_word(self, word):
-        return frozenset(sent for sent, words in self.main_dict.items() if word in words)
+        return {sent for sent, words in self.main_dict.items() if word in words}
 
     def update_WSM(self, n):
         print("update_WSM")
-        for w in self.all_words:
-            for index in range(self.all_words.size):
-                self.latest_WSM[w][index] = self.similarity_W(w, self.all_words[index], n)
+        new = [tuple(self.similarity_W(w, self.all_words[index], n)
+                     for index in range(self.all_words.size))
+               for w in self.all_words]
+
+        self.latest_WSM[:] = new
 
     def update_SSM(self, n):
         print("update_SSM")
-        for s in self.all_sent:
-            for index in range(len(self.all_sent)):
-                self.latest_SSM[s][index] = self.similarity_S(s, self.all_sent[index], n)
+        new = [tuple(self.similarity_S(s, self.all_sent[index], n)
+                     for index in range(self.all_sent.size))
+               for s in self.all_sent]
+
+        self.latest_SSM[:] = new
 
     def WSM(self, n):
         if n > 0:
@@ -169,10 +195,8 @@ class FindSimilarity(Initializer):
 
     def weight(self, **kwargs):
         W, S = kwargs['s'], kwargs['s']
-        counter = Counter(self.all_words)
-        sum5 = sum(j for _, j in counter.most_common(5))
-        word_factor = max(0, 1 - counter[W] / sum5)
-        other_words_factor = sum(max(0, 1 - counter[w] / sum5) for w in self.main_dict[S])
+        word_factor = max(0, 1 - self.counter[W] / self.sum5)
+        other_words_factor = sum(max(0, 1 - self.counter[w] / self.sum5) for w in self.main_dict[S])
         return word_factor / other_words_factor
 
     def iteration(self):
