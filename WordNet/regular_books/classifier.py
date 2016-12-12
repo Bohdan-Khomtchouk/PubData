56106730d3788c7eb797672237d218825db5e69c
Kasra A. Vand <kasraavand@gmail.com>
2016-12-12 11:27:10 +0330

    Read similarity matrices and categorize the words in a json file

--- WordNet/regular_books/classifier.py
+++ WordNet/regular_books/classifier.py
@@ -1,13 +1,47 @@
 import numpy as np
+from itertools import combinations
+from collections import defaultdict
+from operator import itemgetter
+import json
+import re
 import glob
+
+
+regex = re.compile(r'[^_]+_([^.]+)\..*')
+d = defaultdict(dict)
+N = 10
 
 
 def load_files():
     """Load arrays from files."""
     for name in glob.glob("*.npy"):
-        if name.startswith("WSM"):
-            WSM = np.load(name)
-            yield {"name": name, "WSM": WSM}
+        file_name = regex.match(name).group(1)
 
-for result in load_files():
-    WSM = result['WSM']
+        if name.startswith("WSM"):
+            d[file_name]['WSM'] = np.load(name)
+        elif name.startswith("SSM"):
+            d[file_name]['SSM'] = np.load(name)
+        elif name.startswith("sentences"):
+            d[file_name]['sentences'] = np.load(name)
+        elif name.startswith("words"):
+            d[file_name]['words'] = np.load(name)
+
+    for name, matrices in d.items():
+        print(name)
+        WSM = matrices['WSM']
+        words = matrices['words']
+        indices = np.argpartition(WSM, -N, axis=1)[:, -N:]
+        # x = WSM.shape[0]
+        # values = WSM[np.repeat(np.arange(x), N), indices.ravel()].reshape(x, N)
+        # print(values)
+        classified_words = {}
+        for w, inds in zip(words, indices):
+            classified_words[w] = list(words[inds])
+        yield name, classified_words
+
+
+result = load_files()
+
+for name, dict_obj in result:
+    with open("{}.json".format(name), 'w') as f:
+        json.dump(dict_obj, f, indent=4)
