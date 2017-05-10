5db96c38a90a5f1f66e388db4e2d0894153c96f7
kasramvd <kasraavand@gmail.com>
2017-05-10 15:13:23 +0430

    just keep the nouns at final process

--- WordNet/refinecorpus.py
+++ WordNet/refinecorpus.py
@@ -7,6 +7,7 @@
 
 import json
 from nltk.stem import WordNetLemmatizer
+from nltk import pos_tag
 from string import punctuation
 import codecs
 import re
@@ -32,7 +33,7 @@ bio_words = {'cell', 'species', 'blood', 'body', 'group', 'plant', 'water', 'sys
 def refiner(words):
     words = [w.strip(punctuation) for w in words if w not in {'e.g.', 'i.e.'}]
     wnl = WordNetLemmatizer()
-    return [w[0].lower() + w[1:] if w.capitalize() == w else w for w in map(wnl.lemmatize, words)]
+    return [w[0].lower() + w[1:] if w.capitalize() == w else w for w in [wnl.lemmatize(w) for w in words]]
 
 
 bio_words = refiner(bio_words)
@@ -43,28 +44,29 @@ with codecs.open('corpus.json', encoding="UTF-8") as f:
 with open('englishwords.txt') as f:
     words = map(str.strip, f)
 
-new = {}
-for key, value in crp.items():
-    try:
-        key = re.sub(r'[^\s\w/().-]|\(.\)', '', re.sub(u'\u2013', '-', key), re.U)
-    finally:
+    new = {}
+    for key, value in crp.items():
         try:
-            m = re.search(r'([^()]+)(?:\(([^)]+)\))?(.*)', key)
-            k, opt, rest = m.groups()
-        except Exception as exp:
-            print(exp)
-        else:
-            refined = set(refiner(value))
-            preserved = refined.intersection(bio_words)
-            value = list(refined.difference(words).union(preserved))
-            if opt:
-                opt = opt.replace('syn.', '')
-                if '(' in rest:
-                    rest = ' '.join(re.findall(r'\(([^)]+)\)', rest))
-                new[k + ' ' + rest] = value
-                new[opt + ' ' + rest] = value
+            key = re.sub(r'[^\s\w/().-]|\(.\)', '', re.sub(u'\u2013', '-', key), re.U)
+        finally:
+            try:
+                m = re.search(r'([^()]+)(?:\(([^)]+)\))?(.*)', key)
+                k, opt, rest = m.groups()
+            except Exception as exp:
+                print(exp)
             else:
-                new[k] = value
+                refined = set(refiner(value))
+                preserved = refined.intersection(bio_words)
+                value = list(refined.difference(words).union(preserved))
+                if opt:
+                    opt = opt.replace('syn.', '')
+                    if '(' in rest:
+                        rest = ' '.join(re.findall(r'\(([^)]+)\)', rest))
+                    new[k + ' ' + rest] = value
+                    new[opt + ' ' + rest] = value
+                else:
+                    new[k] = value
 
 with codecs.open('corpus_new.json', 'w', encoding='UTF-8') as f:
-    json.dump(new, f, indent=4)
+    refined = {k: [i.lower() for i, tag in pos_tag(v) if tag == 'NN'] for k, v in new.items()}
+    json.dump(refined, f, indent=4)
