0a1a1dfe96bc66106a4f0876f42a6d24c33ff53a
kasramvd <kasraavand@gmail.com>
2017-06-19 15:39:27 +0430

    update model

--- SearchEngine/models.py
+++ SearchEngine/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.utils import timezone
+from djangotoolbox.fields import ListField
 
-# Create your models here.
+
+class Database(models.Model):
+    name = models.CharField(max_length=200)
+    path = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class ServerNames(models.Model):
+	name = models.CharField(max_length=200)
+	path = models.TextField(max_length=200)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+class WordNet(models.Model):
+	word = models.CharField(max_length=200)
+	similars = models.TextField(max_length=200)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
