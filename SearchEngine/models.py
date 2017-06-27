52b91f258f9601c34d183894f2f993b407ca87f2
kasramvd <kasraavand@gmail.com>
2017-06-27 17:14:39 +0430

    create new models and use json for some fields

--- SearchEngine/models.py
+++ SearchEngine/models.py
@@ -1,39 +1,68 @@
 from django.db import models
 from django.utils import timezone
-from djangotoolbox.fields import ListField
+import json
 
 
-class Database(models.Model):
-    name = models.CharField(max_length=200)
-    path = models.TextField()
-    created_date = models.DateTimeField(
+class Server(models.Model):
+    path = models.CharField(max_length=200)
+    files = models.TextField()
+    creation_date = models.DateTimeField(
             default=timezone.now)
 
-    def publish(self):
-        self.published_date = timezone.now()
+    def set_files(self, args):
+        self.files = json.dumps(args)
+
+    def get_files(self):
+        return json.loads(self.foo)
+
+    def add(self):
+        self.creation_date = timezone.now()
         self.save()
 
     def __str__(self):
         return self.title
 
 class ServerNames(models.Model):
-	name = models.CharField(max_length=200)
-	path = models.TextField(max_length=200)
+    name = models.CharField(max_length=200)
+    path = models.CharField(max_length=200)
+    creation_date = models.DateTimeField(
+            default=timezone.now)
 
-    def publish(self):
-        self.published_date = timezone.now()
+    def add(self):
+        self.creation_date = timezone.now()
         self.save()
 
     def __str__(self):
-        return self.title
+        return self.name
+
+    def __repr__(self):
+        return "name: {} | path: {}".format(self.name, self.path)
+
 
 class WordNet(models.Model):
-	word = models.CharField(max_length=200)
-	similars = models.TextField(max_length=200)
+    word = models.CharField(max_length=200)
+    similars = models.TextField()
 
-    def publish(self):
-        self.published_date = timezone.now()
+    def set_similars(self, args):
+        self.files = json.dumps(args)
+
+    def get_similars(self):
+        return json.loads(self.foo)
+
+    def add(self):
+        # self.published_date = timezone.now()
         self.save()
 
-    def __str__(self):
-        return self.title
+class Recommendation(models.Model):
+    user = models.ForeignKey('auth.User')
+    recom = models.TextField()
+
+    def set_recoms(self, args):
+        self.files = json.dumps(args)
+
+    def get_recoms(self):
+        return json.loads(self.foo)
+
+    def add(self):
+        # self.published_date = timezone.now()
+        self.save()
