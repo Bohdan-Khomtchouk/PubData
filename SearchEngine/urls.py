9ae669052b9f64d14b265ff201749cfb8cc8c3c9
kasramvd <kasraavand@gmail.com>
2017-07-01 17:58:28 +0430

    bug fixes and minore improvements

--- SearchEngine/urls.py
+++ SearchEngine/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url(r'^$', views.search, name='search'),
+    url(r'^search_result/(?P<pk>\d+)/', views.search_result, name='search_result'),
 ]
-[]
