1faa31e2387d4b19965b850a686f80ac684a6149
kasramvd <kasraavand@gmail.com>
2017-07-10 22:27:26 +0430

    update choicefield static files and search bar's attributes

--- SearchEngine/forms.py
+++ SearchEngine/forms.py
@@ -7,11 +7,15 @@ class SearchForm(forms.ModelForm):
     class Meta:
         model = Search
         fields = ('word',)
+        widgets = {'word': forms.Textarea(attrs={'rows': 1, 'cols': 2, 'size':4,
+        		  								 'placeholder':"search for data",
+        		  								 'class':'form-control'},),}
 
 
 class SelectServer(forms.Form):
-    servers = forms.ChoiceField(choices = [])
+    servers = forms.ChoiceField(choices=[])
 
     def __init__(self, *args, **kwargs):
-        super(MatchForm, self).__init__(*args, **kwargs)
+        super(SelectServer, self).__init__(*args, **kwargs)
         self.fields['servers'].choices = [(x.name, x.path) for x in ServerNames.objects.all()]
+        self.fields['servers'].widget.attrs['class'] = "dropdown show"
