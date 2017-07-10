13f665dd76a0228b9dcbdd56682a49b0d0511e67
kasramvd <kasraavand@gmail.com>
2017-07-10 22:27:52 +0430

    update static files

---
+++ SearchEngine/static/js/searchengine.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "70%";
+    // document.getElementById("flipkart-navbar").style.width = "50%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.body.style.backgroundColor = "rgba(0,0,0,0)";
+}
