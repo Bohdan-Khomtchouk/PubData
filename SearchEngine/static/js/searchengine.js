63fe46871ef6d3d9d93c4a946b0bda8e3aeee675
kasramvd <kasraavand@gmail.com>
2017-07-12 17:33:19 +0430

    add jquery file for multiple select file and update search call function

--- SearchEngine/static/js/searchengine.js
+++ SearchEngine/static/js/searchengine.js
@@ -8,3 +8,33 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.body.style.backgroundColor = "rgba(0,0,0,0)";
 }
+
+function search_result() {
+	choiceform = document.getElementById('pre-selected-options')
+    var listLength = choiceform.length;
+    var selected = []
+    for(var i=0;i<listLength;i++){
+        if(choiceform[i].selected)
+            selected.push(choiceform[i].value);
+        }
+    var search_query = $('submit_search').val()
+    $.ajax({
+        url : "search_result/" + search_query, // the endpoint
+        type : "POST", // http method
+        data : { 'search' : search_query, 'selected': selected }, // data sent with the post request
+
+        // handle a successful response
+        success : function(json) {
+            //$('#post-text').val(''); // remove the value from the input
+            console.log(json); // log the returned json to the console
+            console.log("success"); // another sanity check
+        },
+
+        // handle a non-successful response
+        error : function(xhr, errmsg, err) {
+            $('#results').html("<div class='alert-box alert radius' data-alert>Oops! We have encountered an error: "+errmsg+
+                " <a href='#' class='close'>&times;</a></div>"); // add the error to the dom
+            console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+        }
+    });
+};
