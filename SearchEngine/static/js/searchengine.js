a3bfb74bbe8a0dc664847a99d0b6e5ef5f85ce90
kasramvd <kasraavand@gmail.com>
2017-07-13 22:56:54 +0430

    update search_result ajax request and add csrf tokenizer support

--- SearchEngine/static/js/searchengine.js
+++ SearchEngine/static/js/searchengine.js
@@ -8,20 +8,43 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.body.style.backgroundColor = "rgba(0,0,0,0)";
 }
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
 
 function search_result() {
-	choiceform = document.getElementById('pre-selected-options')
-    var listLength = choiceform.length;
+	ms = document.getElementById('pre-selected-options');
+    var options = $('#pre-selected-options option:selected');
     var selected = []
-    for(var i=0;i<listLength;i++){
-        if(choiceform[i].selected)
-            selected.push(choiceform[i].value);
+    for(var i=0;i<options.length;i++){
+            selected.push(options[i].value);
         }
-    var search_query = $('submit_search').val()
+    var search_query = document.getElementById('id_word').value;
+    var csrftoken = getCookie('csrftoken');
     $.ajax({
+        dataType: "json",
         url : "search_result/" + search_query, // the endpoint
         type : "POST", // http method
-        data : { 'search' : search_query, 'selected': selected }, // data sent with the post request
+        data : { 'keyword' : search_query,
+                 'selected': JSON.stringify(selected),
+                 'csrfmiddlewaretoken': csrftoken }, // data sent with the post request
 
         // handle a successful response
         success : function(json) {
@@ -37,4 +60,11 @@ function search_result() {
             console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
         }
     });
+    $.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
 };
