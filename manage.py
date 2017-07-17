5b6c4485d6e1a2a2870af873729d9433f127072c
kasramvd <kasraavand@gmail.com>
2017-07-17 16:07:30 +0430

    bug fixes and refine typos

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import django
 
 
 if __name__ == "__main__":
-    run_initializer = True
+    run_initializer = False
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PubData.settings")
     try:
         from django.core.management import execute_from_command_line
