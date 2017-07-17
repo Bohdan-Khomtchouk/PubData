75c89657e4f2064717af9cd70f60c626d52a1139
kasramvd <kasraavand@gmail.com>
2017-07-17 16:05:06 +0430

    update __str__ method of the dyamic models and catch the ProgrammingError exceptions while creating relations

--- SearchEngine/lib/create_tables.py
+++ SearchEngine/lib/create_tables.py
@@ -3,8 +3,9 @@ from django.db import models
 import django
 import os
 import json
-from django.core.management import call_command
+# from django.core.management import call_command
 from django.db import connection
+from django.db.utils import ProgrammingError
 
 
 def create_servers(server_names):
@@ -16,19 +17,24 @@ def create_servers(server_names):
     cache = {}
     for name in server_names:
         model_class = create_model(name,
-                                   fields={'path': models.CharField(max_length=200),
+                                   fields={'path': models.TextField(),
                                            'file_names': models.TextField(),
                                            'set_file_names': set_file_names,
                                            'get_file_names': get_file_names,
-                                           '__str__': lambda self: self.__name__},
+                                           '__str__': lambda self: self.path,
+                                           '__name__': name},
                                    app_label='SearchEngine',
                                    module='SearchEngine.models',
                                    options=None,
                                    admin_opts={})
 
         cache[name] = model_class
-        # with connection.schema_editor() as editor:
-        #    editor.create_model(model_class)
+        try:
+            with connection.schema_editor() as editor:
+                editor.create_model(model_class)
+        except ProgrammingError:
+            print("relation {} exist.".format(name))
+
     return cache
 
 if __name__ == '__main__':
