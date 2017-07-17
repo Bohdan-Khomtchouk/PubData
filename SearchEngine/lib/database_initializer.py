acbe7a9ad453b35eb2077d83309cb5782544db4c
kasramvd <kasraavand@gmail.com>
2017-07-17 16:06:12 +0430

    use bulk_create method for bulk insert to database for better optimization

--- SearchEngine/lib/database_initializer.py
+++ SearchEngine/lib/database_initializer.py
@@ -22,21 +22,26 @@ class Initializer:
         self.add_servers()
 
     def add_wordnets(self): 
-        query = WordNet()
+        all_models = []
         for word, similars in self.load_wordnets().items():
+            query = WordNet()
             query.word = word
             query.set_similars(similars)
-            query.save()
+            all_models.append(query)
+        WordNet.objects.bulk_create(all_models)
 
     def add_servers(self):
         servers = create_servers(server_names=self.server_names)
         for name, file in self.load_servers():
             model_ = servers[name]
-            query = model_()
+            all_models = []
             for path, file_names in file.items():
+                query = model_()
                 query.path = path
                 query.set_file_names(file_names)
-                query.save()
+                all_models.append(query)
+
+            model_.objects.bulk_create(all_models)
 
     def add_server_names(self):
         query = ServerName()
@@ -44,7 +49,6 @@ class Initializer:
             if name not in self.excluded_names:
                 query.name = name
                 query.path = url
-                query.set_table_field(name)
                 query.add()
 
     def load_server_names(self):
