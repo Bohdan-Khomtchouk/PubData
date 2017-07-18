e582d1274c79ce76a63972c76d6b763dd8d92c1c
kasramvd <kasraavand@gmail.com>
2017-07-18 18:02:32 +0430

    add search function

--- SearchEngine/lib/utils.py
+++ SearchEngine/lib/utils.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib import admin
+from sys import path as syspath
+from os import path as ospath
+syspath.append(ospath.join(ospath.expanduser("~"), 'PubData'))
+from SearchEngine.models import Server
 
 
-def search():
-    pass
+def find_search_result(**kwargs):
+    keyword = kwargs['keyword']
+    servers = kwargs['servers']
