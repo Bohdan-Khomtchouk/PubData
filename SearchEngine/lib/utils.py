6b433000e15e9fdf1b4b21075233bdeb31a7e7c9
kasramvd <kasraavand@gmail.com>
2017-07-23 16:20:08 +0430

    update find_result function

--- SearchEngine/lib/utils.py
+++ SearchEngine/lib/utils.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from sys import path as syspath
 from os import path as ospath
 from django.db.models import Q
+from collections import defaultdict
 syspath.append(ospath.join(ospath.expanduser("~"), 'PubData'))
 from SearchEngine.models import Server, WordNet
 
@@ -13,22 +14,24 @@ class FindSearchResult:
         self.selected_servers = kwargs['servers']
 
     def find_result(self):
-        query = Server()
+        # query = Server()
         # query.objects.filter(name__in=selected_servers)
-        all_servers = {name: query.objects.get(name=name) for name in self.selected_servers}
-        wordnet_result = self.get_similars()
-        for name, server in all_servers:
+        all_servers = {name: Server.objects.get(name=name) for name in self.selected_servers}
+        wordnet_result = list(self.get_similars())
+        for name, server in all_servers.items():
+            result = defaultdict(list)
             for path, files in server.data.items():
-                for word in wordnet_result:
-                    if any(word in f_name for f_name in files):
-                        yield {'server': name,
-                               'path': path,
-                               'exact_match': self.keyword == word}
+                for wn in wordnet_result:
+                    if wn['word'] in files or wn['similars'].intersection(files):
+                        result[name].append({'paths': path,
+                                             'exact_match': self.keyword == wn['word']})
+            yield result
 
     def get_similars(self):
-        wordnet = WordNet()
-        obj = wordnet.objects.filter(
+        # wordnet = WordNet()
+        all_obj = WordNet.objects.filter(
             Q(similars__contains=[self.keyword]) | Q(word=self.keyword)
         )
-        if obj:
-            return {'word': obj.word, 'similars': obj.similars}
+
+        for obj in all_obj:
+            yield {'word': obj.word, 'similars': set(obj.similars)}
