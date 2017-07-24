42c79e9839b8e85147ab614133d949df75154e72
kasramvd <kasraavand@gmail.com>
2017-07-24 23:26:44 +0430

    update search function

--- SearchEngine/lib/utils.py
+++ SearchEngine/lib/utils.py
@@ -6,6 +6,7 @@ from django.db.models import Q
 from collections import defaultdict
 syspath.append(ospath.join(ospath.expanduser("~"), 'PubData'))
 from SearchEngine.models import Server, WordNet
+from string import punctuation, whitespace
 
 
 class FindSearchResult:
@@ -13,20 +14,36 @@ class FindSearchResult:
         self.keyword = kwargs['keyword']
         self.selected_servers = kwargs['servers']
 
+    def validate_keyword(self):
+        punctuation = set(punctuation) - {'-', '_'}
+        cond1 = len(self.kwargs) > 1
+        cond2 = not punctuation.intersection(self.keyword)
+        cond3 = not set(whitespace).intersection(self.keyword)
+
+        return cond1 and cond2 and cond3
+
     def find_result(self):
         # query = Server()
         # query.objects.filter(name__in=selected_servers)
+        if not self.validate_keyword():
+            # should raise exception
+            return []
         all_servers = {name: Server.objects.get(name=name) for name in self.selected_servers}
-        wordnet_result = list(self.get_similars())
+        wordnet_result = self.get_similars()
         for name, server in all_servers.items():
             result = defaultdict(list)
             for path, files in server.data.items():
+                print ([path, files])
                 for wn in wordnet_result:
-                    if wn['word'] in files or wn['similars'].intersection(files):
+                    print(wn)
+                    if self.check_intersection(wn, files):
                         result[name].append({'paths': path,
                                              'exact_match': self.keyword == wn['word']})
             yield result
 
+    def check_intersection(self, wn, files):
+        if any(wn['word'] for f in files) or wn['similars'].intersection(files):
+    
     def get_similars(self):
         # wordnet = WordNet()
         all_obj = WordNet.objects.filter(
