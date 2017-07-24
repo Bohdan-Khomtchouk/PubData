0b1a77035d517b39959c261aa1b8aface9137bcc
kasramvd <kasraavand@gmail.com>
2017-07-24 23:28:10 +0430

    remove extra url

--- SearchEngine/urls.py
+++ SearchEngine/urls.py
@@ -4,8 +4,4 @@ from . import views
 urlpatterns = [
     url(r'^$', views.search, name='search'),
     url(r'^search_result/.+$', views.search_result, name='search_result'),
-    url(r'^new_search_result/(?P<result>.*)$',
-        views.new_search_result,
-        kwargs={'result': ''},
-        name='new_search_result')
 ]
