22611cdde3e00aab4d807c48169f0155a2ef5aaa
kasramvd <kasraavand@gmail.com>
2017-07-27 01:41:19 +0430

    add signup url

--- SearchEngine/urls.py
+++ SearchEngine/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.search, name='search'),
     url(r'^search_result/.+$', views.search_result, name='search_result'),
+    url(r'^signup/', views.signup_view, name='signup'),
 ]
