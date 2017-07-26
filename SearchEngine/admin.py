7094b325d963b98d1223a30b247f617d3b502bb2
kasramvd <kasraavand@gmail.com>
2017-07-26 12:08:24 +0430

    refine the syntax

--- SearchEngine/admin.py
+++ SearchEngine/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from . import models
 
 for name in dir(models):
-	obj = getattr(models, name)
-	if type(obj).__name__ == "ModelBase":
-		admin.site.register(obj)
+    obj = getattr(models, name)
+    if type(obj).__name__ == "ModelBase":
+        admin.site.register(obj)
