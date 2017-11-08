52cc87b78555e5952f0010af34a3ffa05b039803
kasramvd <kasraavand@gmail.com>
2017-11-08 19:00:52 +0330

    update query sets security bug fix and performance improvements

--- SearchEngine/lib/utils.py
+++ SearchEngine/lib/utils.py
@@ -21,16 +21,14 @@ class FindSearchResult:
         punct = set(punctuation) - {'-', '_'}
         cond1 = len(self.keyword) > 1
         cond2 = not punct.intersection(self.keyword)
-        cond3 = not set(whitespace).intersection(self.keyword)
 
-        result = cond1 and cond2 and cond3
+        result = cond1 and cond2
         if result:
-            lst = re.split(r'(-|_|\s)', self.keyword)
-            all_subs = {''.join(lst[i:j]) for i in range(0, len(lst) - 2, 2)
-                        for j in range(i + 3, len(lst) + 2, 2)}
-            self.all_valid_substrings = list(all_subs | {self.keyword})
-            splitted_substrings = re.split(r'-|_', self.keyword)
-            if len(splitted_substrings) > 1:
+            lst = set([i for i in re.split(r'-|_', self.keyword) if i][:5])
+            # all_subs = {''.join(lst[i:j]) for i in range(0, len(lst) - 2, 2)
+            #            for j in range(i + 3, len(lst) + 2, 2)}
+            splitted_substrings = list(lst | {self.keyword})
+            if len(splitted_substrings) >= 1:
                 self.splitted_substrings = splitted_substrings
             else:
                 self.splitted_substrings = []
@@ -41,33 +39,35 @@ class FindSearchResult:
         # query.objects.filter(name__in=selected_servers)
         if not self.validate_keyword():
             raise ValueError("Invalid Keyword")
-        all_result = {}
         wordnet_result = self.get_similars()
         all_words = wordnet_result['words'] & wordnet_result['similars']
+        all_words = list(all_words) + self.splitted_substrings
         for name, url in self.selected_servers.items():
             cond1 = Q(server_name=name)
-            # cond2 = Q(files__overlap=list(wordnet_result['similars']))
-            # cond3 = Q(files__overlap=list(wordnet_result['words']))
+            # these conditions take too long
+            # cond2 = Q(files__overlap=all_words)
+            # cond3 = Q(path__icontains=all_words)
             query_result = Path.objects.filter(cond1)  # & (cond2 | cond3))
             for obj in query_result:
-                if self.check_intersection(obj, all_words):
+                if self.check_intersection(obj.files, obj.path, all_words):
                     yield {'path': obj.path,
                            'name': name,
                            'url': url,
-                           'exact_match': self.keyword in wordnet_result['words']}
+                           'exact_match':self.exact_match(obj.files, obj.path) }
 
-        #return all_result
-
-    def check_intersection(self, obj, all_words):
+    
+    def exact_match(self, files, path):
+        return any(i in files or i in path for i in self.splitted_substrings)
+    
+    def check_intersection(self, files, path, all_words):
         # this should be done in json files
-        return any(self.keyword in word or any(i in word for i in self.splitted_substrings)
-                   for word in all_words)
-
+        all_words = list(all_words) + self.splitted_substrings 
+        return set(files).intersection(all_words) or any(i in path for i in all_words)
+    
     def get_similars(self):
-        cond1 = Q(similars__overlap=self.all_valid_substrings)
-        cond2 = Q(word__in=self.all_valid_substrings)
-        cond3 = Q(similars__overlap=self.splitted_substrings)
-        all_obj = WordNet.objects.filter(cond1 | cond2 | cond3)
+        cond1 = Q(similars__overlap=self.splitted_substrings)
+        cond2 = Q(word__in=self.splitted_substrings)
+        all_obj = WordNet.objects.filter(cond1 | cond2)
         words = {obj.word for obj in all_obj}
         similars = {i for obj in all_obj for i in obj.similars}
         return {'words': words, 'similars': similars}
