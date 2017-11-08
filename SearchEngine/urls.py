ef83fce91b8ef6a6cf1bf28ee2d11e5b6889fc31
kasramvd <kasraavand@gmail.com>
2017-11-08 16:13:26 +0330

    update urls

--- SearchEngine/urls.py
+++ SearchEngine/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.search, name='search'),
-    url(r'^search_result/.+$', views.search_result, name='search_result'),
+    url(r'^search_result/.*$', views.search_result, name='search_result'),
+    #url(r'^result/(?P<page>\d+)?.*$', views.change_page, name='change_page'),
     url(r'^signup/', views.signup_view, name='signup'),
 ]
