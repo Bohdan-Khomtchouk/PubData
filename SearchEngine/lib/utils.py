363866726c899fc80db42ebd938a1da14223999c
kasramvd <kasraavand@gmail.com>
2017-11-12 12:25:08 +0330

    search through the file names and improve perfromance

--- SearchEngine/lib/utils.py
+++ SearchEngine/lib/utils.py
@@ -16,6 +16,8 @@ class FindSearchResult:
     def __init__(self, *args, **kwargs):
         self.keyword = kwargs['keyword'].strip('-_')
         self.selected_servers = kwargs['servers']
+        self.splitted_substrings = []
+        self.punc_regex = re.compile(r'[{}]'.format(re.escape(punctuation)))
 
     def validate_keyword(self):
         punct = set(punctuation) - {'-', '_'}
@@ -23,15 +25,14 @@ class FindSearchResult:
         cond2 = not punct.intersection(self.keyword)
 
         result = cond1 and cond2
-        if result:
+        if cond1 and cond2:
             lst = set([i for i in re.split(r'-|_', self.keyword) if i][:5])
             # all_subs = {''.join(lst[i:j]) for i in range(0, len(lst) - 2, 2)
             #            for j in range(i + 3, len(lst) + 2, 2)}
             splitted_substrings = list(lst | {self.keyword})
             if len(splitted_substrings) >= 1:
                 self.splitted_substrings = splitted_substrings
-            else:
-                self.splitted_substrings = []
+
         return result
 
     def find_result(self):
@@ -41,7 +42,7 @@ class FindSearchResult:
             raise ValueError("Invalid Keyword")
         wordnet_result = self.get_similars()
         all_words = wordnet_result['words'] & wordnet_result['similars']
-        all_words = list(all_words) + self.splitted_substrings
+        all_words = all_words.union(self.splitted_substrings)
         for name, url in self.selected_servers.items():
             cond1 = Q(server_name=name)
             # these conditions take too long
@@ -61,8 +62,8 @@ class FindSearchResult:
     
     def check_intersection(self, files, path, all_words):
         # this should be done in json files
-        all_words = list(all_words) + self.splitted_substrings 
-        return set(files).intersection(all_words) or any(i in path for i in all_words)
+        files = {i for f in files for i in self.punc_regex.split(f)}
+        return all_words.intersection(files) or any(i in path for i in all_words)
     
     def get_similars(self):
         cond1 = Q(similars__overlap=self.splitted_substrings)
